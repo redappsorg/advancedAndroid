a40158d2b18b5609543b35de3497dc3474c03b75
redappsorg <62048698+redappsorg@users.noreply.github.com>
2020-03-22 23:56:44 +0430

    Add files via upload

---
+++ 09_HTTPBackgroundTasks/PhotoGallery/app/src/main/java/org/redapps/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,11 @@
+package org.redapps.photogallery;
+
+import android.support.v4.app.Fragment;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
+    }
+}
